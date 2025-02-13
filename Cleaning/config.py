dcab6a5d7e2b2dc191a2490fbae4e339ac3e3ef0
vhp201003 <xcuad001234@gmail.com>
2025-02-13 17:58:49 +0700

    Update cleaning config

--- Cleaning/config.py
+++ Cleaning/config.py
@@ -1,10 +1,12 @@
 import os
 
+CLEANING_FOLDER = r"../Data/cleaning/"
+
 INPUT_FILE_PATH = r"../Data/crawling/comments_data.csv"
-REMOVE_URL_EMOJI_OUTPUT_PATH = r"../Data/cleaning/remove_url_emoj/remove_url_emoj.csv"
-CHUNKING_BEFORE_MODEL_FILE_PATH = r"../Data/cleaning/chunk_input/"
-CHUNKING_AFTER_MODEL_FILE_PATH = r"../Data/cleaning/chunk_output/"
-OUTPUT_FILE_PATH = r"../Data/cleaning/output/"
+REMOVE_URL_EMOJI_OUTPUT_PATH = CLEANING_FOLDER + r"remove_url_emoj/remove_url_emoj.csv"
+CHUNKING_BEFORE_MODEL_FILE_PATH = CLEANING_FOLDER + r"chunk_input/"
+CHUNKING_AFTER_MODEL_FILE_PATH = CLEANING_FOLDER + r"chunk_output/"
+OUTPUT_FILE_PATH = CLEANING_FOLDER + r"output/"
 OUTPUT_MERGED_FILE_NAME = r"output_merged_model.csv"
 OUTPUT_REMOVE_PUNCTUATION_FILE_NAME = r"output_remove_punctuation.csv"
 OUTPUT_CLEANING_FILE_NAME = r"cleaning.csv"
